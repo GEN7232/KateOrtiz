20a03d7743e764c11c213383452c4c5739be50d1
Genevieve Nelson <GEN7232@gmail.com>
2026-07-23 15:47:19 -0400

    removed react player from videos and replaced with youtube iframe

--- src/components/pages/Music/Videos.js
+++ src/components/pages/Music/Videos.js
@@ -1,6 +1,30 @@
 import React from "react";
-import ReactPlayer from "react-player";
-import "./Videos.css";
+
+const videos = [
+  {
+    title: '"Just Like Poison" Lyric Video',
+    url: "https://www.youtube.com/embed/XlJEfVw0Ypc?si=JZhQgDswbO3dk8EJ",
+    description: 'Vibey lyric video for "Just Like Poison"',
+  },
+  {
+    title: '"Just Like Poison" Live',
+    url: "https://www.youtube.com/embed/eVgK0L1-dr0?si=1Re1QdyL08d3W5LK",
+    description:
+      'Live performance video of "Just Like Poison" recorded at Lakehouse Recording Studios as part of their Lakehouse Live series. Featuring Genevieve Nelson on bass and Maria Ruiz on drums.',
+  },
+  {
+    title: '"Shine" Live',
+    url: "https://www.youtube.com/embed/LPZT373hIKc?si=imUFqCVhnvqDCN2S",
+    description:
+      'Live performance video of "Shine" recorded at Lakehouse Recording Studios as part of their Lakehouse Live series. Featuring Genevieve Nelson on bass and Maria Ruiz on drums.',
+  },
+  {
+    title: '"Apologize" Live',
+    url: "https://www.youtube.com/embed/Z2fV8bj53C8?si=0ZrPKVAs6UqF7nG1",
+    description:
+      'Live performance video recorded at Lakehouse Recording Studios. Featuring Genevieve Nelson on bass, Rob Tanico on guitar and background vocals, and K Kiatpreecha on drums.',
+  },
+];
 
 const Videos = () => {
   return (
@@ -16,82 +40,30 @@ const Videos = () => {
       </header>
       <div>
         <div className="videos">
-          <div className="videos-flex">
-            <div className="player-wrapper">
-              <h2>"Just Like Poison" Lyric Video</h2>
-              <ReactPlayer
-                className="react-player react-player-poison"
-                url="https://www.youtube.com/watch?v=XlJEfVw0Ypc"
-                height="100%"
-                width="100%"
-              />
+          {videos.map((video) => (
+            <div className="individual-video-with-description" key={video.title}>
+              <div className="player">
+                <h2>{video.title}</h2>
+                    <iframe
+                      src={video.url}         
+                      title={video.title}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"
+                      allowFullScreen
+                    />
+              </div>
+              <div className="descriptions">
+                <p>{video.description}</p>
+              </div>
+              <hr></hr>
             </div>
-            <div className="poison-video">
-              <p>Vibey lyric video for "Just Like Poison"</p>
-            </div>
-          </div>
-          <hr></hr>
-          <div className="videos-flex">
-            <div className="player-wrapper">
-              <h2>"Just Like Poison" Live</h2>
-              <ReactPlayer
-                className="react-player react-player-poison"
-                url="https://www.youtube.com/watch?v=eVgK0L1-dr0"
-                height="100%"
-                width="100%"
-              />
-            </div>
-            <div className="poison-video">
-              <p>
-                Live performance video of "Just Like Poison" recorded at
-                Lakehouse Recording Studios as part of their Lakehouse Live
-                series. Featuring Genevieve Nelson on bass and Maria Ruiz on
-                drums.
-              </p>
-            </div>
-          </div>
-          <hr></hr>
-          <div className="videos-flex">
-            <div className="player-wrapper">
-              <h2>"Shine" Live</h2>
-              <ReactPlayer
-                className="react-player react-player-shine"
-                url="https://www.youtube.com/watch?v=LPZT373hIKc"
-                height="100%"
-                width="100%"
-              />
-            </div>
-            <div className="shine-video">
-              <p>
-                Live performance video of "Shine" recorded at Lakehouse
-                Recording Studios as part of their Lakehouse Live series.
-                Featuring Genevieve Nelson on bass and Maria Ruiz on drums.
-              </p>
-            </div>
-          </div>
-          <hr></hr>
-          <div className="videos-flex">
-            <div className="player-wrapper">
-              <h2>"Apologize" Live</h2>
-              <ReactPlayer
-                className="react-player react-player-apologize"
-                url="https://www.youtube.com/watch?v=Z2fV8bj53C8"
-                height="100%"
-                width="100%"
-              />
-            </div>
-            <div className="apologize-video">
-              <p>
-                Live performance video recorded at Lakehouse Recording Studios.
-                Featuring Genevieve Nelson on bass, Rob Tanico on guitar and
-                background vocals, and K Kiatpreecha on drums.
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
   );
-};
+}
+
 
 export default Videos;
