54909ee342d046b74a7e2a3169c37337d771e503
Genevieve Nelson <GEN7232@gmail.com>
2026-07-23 16:18:49 -0400

    mapped iframes in commissions.js

--- src/components/pages/Music/Commissions.js
+++ src/components/pages/Music/Commissions.js
@@ -1,5 +1,30 @@
 import React from "react";
-import ReactPlayer from "react-player";
+
+const podcasts = [
+  {
+    title: "Band Battles Intro",
+    url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1562031628&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    description: "Intro music for the Band Battles podcast",
+  },
+  {
+    title: "Band Battles Outro with VO",
+    url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1562029693&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    description: "Outro music for the Band Battles podcast with voiceover",
+  },
+  {
+    title: "Band Battles Outro without VO",
+    url: "https://www.youtube.com/embed/qNQ8K21sstE?si=OcAb93rgVGe8-AJ_",
+    description: "Outro music for the Band Battles podcast without voiceover",
+  },
+];
+
+const videoCommissions = [
+  {
+    title: "Video Background",
+    url: "https://www.youtube.com/embed/V3yGhIc0y2c?si=R0BEAsw4zGHn2Vcl",
+    description: "Kate wrote the backing music for this video.",
+  },
+];
 
 const Commissions = () => {
   return (
@@ -14,37 +39,45 @@ const Commissions = () => {
           Kate Ortiz wrote and performed the instruments for the following
           podcast intros/outros
         </p>
-        <div className="player-wrapper">
-          <ReactPlayer
-            url="https://soundcloud.com/user-534750572/band-battles-intro?si=9ec94c1c94f8478fa0e4e5768de041fb&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-            className="react-player1"
-          />
-          <ReactPlayer
-            url="https://soundcloud.com/user-534750572/band-battles-outro-with-vo?si=ca225b9d25414de4a3a49c6a87f52e9e&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-            className="react-player2"
-
-          />          
-
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=qNQ8K21sstE"
-            className="react-player3"
-
-          />
-        </div>
+        {podcasts.map((podcast) => (
+          <div
+            className="player individual-podcast-with-description"
+            key={podcast.title}
+          >
+            <iframe
+              src={podcast.url}
+              title={podcast.title}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
       </div>
       <hr></hr>
       <div className="video-commissions">
         <div className="video-desc">
           <h2>Video Background</h2>
           <br></br>
-          <p>Kate wrote the backing music for the following video.</p>
         </div>
-        <div className="player-wrapper">
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=V3yGhIc0y2c"
-            className="react-player4"
-          />
-        </div>
+        {videoCommissions.map((video) => (
+          <div className="individual-video-with-description" key={video.title}>
+            <div className="descriptions">
+              <p>{video.description}</p>
+            </div>
+            <div className="player">
+              <iframe
+                src={video.url}
+                title={video.title}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
