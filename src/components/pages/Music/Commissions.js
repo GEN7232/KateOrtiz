60da1de4a0f152fea7f08871790b000b72bc9bcf
Genevieve Nelson <GEN7232@gmail.com>
2026-08-08 18:56:06 -0400

    Added Big Time End credits link

--- src/components/pages/Music/Commissions.js
+++ src/components/pages/Music/Commissions.js
@@ -24,6 +24,11 @@ const videoCommissions = [
     url: "https://www.youtube.com/embed/V3yGhIc0y2c?si=R0BEAsw4zGHn2Vcl",
     description: "Kate wrote the backing music for this video.",
   },
+  {
+    title: "'Nearly the Big Time' Mike Spade Magic Special End Credits",
+    url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2378091899&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    description: "End credits for the 'Nearly the Big Time' Mike Spade Magic Special",
+  },
 ];
 
 const Commissions = () => {
